267231bcaaf16b966be718a16165d18d0a0e9a03
An Nguyen <ntruongant1@gmail.com>
2023-08-31 20:51:00 +0700

    refactor

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import classnames from 'classnames';
+
+export const NavBar = () => {
+  const isLoggedIn = true;
+  return (
+    <div className={classnames(styles.navbarWrapper)}>
+      <div className={classnames(styles.navbarItems)}>
+        <div className={classnames(styles.navbarItem, styles.dropdown)}>
+          Danh sách lớp
+          <div className={classnames(styles.dropdownContent)}>
+            <a href='#'>Các lớp cấp 1</a>
+            <a href='#'>Các lớp cấp 2</a>
+            <a href='#'>Các lớp cấp 3</a>
+            <a href='#'>Các lớp ngoại ngữ</a>
+          </div>
+        </div>
+        <div className={classnames(styles.navbarItem, styles.hoverPointer)}>
+          Đăng ký gia sư
+        </div>
+        <div className={classnames(styles.navbarItem, styles.hoverPointer)}>
+          Thông tin
+        </div>
+        <div className={classnames(styles.navbarItem, styles.hoverPointer)}>
+          Liên hệ
+        </div>
+      </div>
+      {isLoggedIn ? (
+        <div className={classnames(styles.accountInformation)}>
+          <div className={classnames(styles.loginButton)}>
+            Thông tin tài khoản
+          </div>
+          <div className={classnames(styles.logoutButton)}>Đăng xuất</div>
+        </div>
+      ) : (
+        <div className={classnames(styles.accountInformation)}>
+          <div className={classnames(styles.loginButton)}>Đăng nhập</div>
+          <div className={classnames(styles.signUpButton)}>Đăng ký</div>
+        </div>
+      )}
+    </div>
+  );
+};
